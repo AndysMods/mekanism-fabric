1377e3cd490dacfed2e144fee24abb7206dabbc2
Andy608 <Andy608@users.noreply.github.com>
2024-05-14 09:02:28 -0700

    Initial commit. Set up a basic fabric repository for Mekanism 1.20.1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mekanism-fabric
+
+Casually attempting to port Mekanism over to Fabric for Minecraft 1.20.1
---
+++ src/main/java/com/andysmods/mekanism/MekanismDataGenerator.java
@@ -0,0 +1,11 @@
+package com.andysmods.mekanism;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MekanismDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
